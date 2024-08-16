e9921ea8c453a7230327dc25c5c29c6aa55116da
Vamaraju Sai Rama Charan <124505291+Ramcharan-19@users.noreply.github.com>
2024-08-16 16:24:20 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is not a front-end technology?",
+    answer: "SQL",
+    options: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "SQL"
+    ]
+  },
+    {
+    numb: 2,
+    question: " The purpose of the Front-end framework in Full stack development is :",
+    answer: "To provide the client-side interface",
+    options: [
+      "To provide the client-side interface",
+      "To manage databse",
+      "To reduce the server load",
+      "To send http requests"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Of the following sort algorithms, which has execution time that is least dependent on initial ordering of the input ?",
+    answer: "Merge sort",
+    options: [
+      "Selection sort",
+      "Insertion sort",
+      "Quick sort",
+      "Merge sort"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Database in Full stack development is used to :",
+    answer: "Storing and retrieving data",
+    options: [
+      "Styling HTML pages",
+      "Storing and retrieving data",
+      "Handling errors on server-side",
+      "Rendering web pages"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which data structure is used for implementing recursion?",
+    answer: "Stack",
+    options: [
+      "Stack",
+      "Queue",
+      "List",
+      "Array"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which of the following is not the application of stack?",
+    answer: "Data Transfer between two asynchronous process",
+    options: [
+      "A parentheses balancing program",
+      "Compiler Syntax Analyzer",
+      "Tracking of local variables at run time",
+      "Data Transfer between two asynchronous process",
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which data structure is needed to convert infix notation to postfix notation?",
+    answer: "Stack",
+    options: [
+      "Tree",
+      "Branch",
+      "Stack",
+      "Queue",
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of the following application makes use of a circular linked list?",
+    answer: "Allocating CPU to resources",
+    options: [
+      "Recursive function calls",
+      "Undo operation in a text editor",
+      "Implement Hash Tables",
+      "Allocating CPU to resources",
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is the purpose of CSS in Full stack development?",
+    answer: "To style and format HTML elements",
+    options: [
+      "To manage http request and response",
+      "To store and retrieve data",
+      "To style and format HTML elements",
+      "None of the above",
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which HTML tag is used to create vector graphics and illustrations?",
+    answer: "svg",
+    options: [
+      "canvas",
+      "video",
+      "svg",
+      "details",
+    ]
+  },
+];
+
